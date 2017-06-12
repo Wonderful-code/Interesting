d2a0a519acf06992be275e787e05bb847366e32b
Wonderful-code <www.237261546@qq.com>
2017-06-12 09:49:54 +0800

    Interesting
    集中开发 脸部识别与ui界面部分

---
+++ interesing_view.py
@@ -0,0 +1,169 @@
+#interesing_view.py
+import cv2
+import time
+import pygame
+import imutils
+import argparse
+import datetime
+import random
+import numpy as np
+from interesion_model import exciting
+from imutils.object_detection import non_max_suppression
+from pygame.locals import *
+from sys import exit
+
+# 创建参数解析器并解析参数
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-i", "--image", help="path to the image file")
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-n", "--name", type = str, default="Capture",help="window name")
+ap.add_argument("-w", "--width", type = int,default=800,help="window width")
+ap.add_argument("-ht", "--height", type = int,default=1000,help="window height")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+
+args = vars(ap.parse_args())
+# 如果video参数为None，那么我们从摄像头读取数据
+if args.get("video", None) is None and args["image"] is None:
+    camera = cv2.VideoCapture(0)
+    #等待0.25秒
+    time.sleep(0.25)
+ 
+# 否则我们读取一个视频文件
+else:
+    camera = cv2.VideoCapture(args["video"])
+
+width = args["width"]
+height = args["height"]
+frames = 0 #帧计数器
+history = 20 #背景建模样本量
+#firstFrame = None
+backgrouds = []
+pedestrians = {} #行人字典
+
+pygame.init()
+pygame.display.set_caption('Interesing')
+screen = pygame.display.set_mode((width+200,height-399),pygame.RESIZABLE)
+screen.fill([0,0,0])#用黑色填充窗口
+et=exciting(camera,history)
+#et.read_images_array()
+
+backgrouds = et.readBackgroud(random.randint(0,19))
+if backgrouds != []:
+	frames = 20
+
+faces = [['陈思羽','18']]
+faceShow = []
+
+f=0
+a=0
+sp=0
+#img= cv2.imread('face/face_gray/',cv2.IMREAD_GRAYSCALE)
+while camera.isOpened():
+
+	time = datetime.datetime.now().strftime(u"%Y-%d %I:%M:%S")
+	(ret,cv_img)= camera.read()
+	frame = imutils.resize(cv_img,width,height)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	#opencv的色彩空间是BGR，pygame的色彩空间是RGB
+	img = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+	et.show_text(screen,(10,frame.shape[0]-40),time,(0,131,195),True,30)
+	pygame.display.update()
+
+	pixl_arr = np.swapaxes(img, 0, 1)
+	new_surf = pygame.pixelcopy.make_surface(pixl_arr)
+	#设置窗口背景
+	screen.blit(new_surf, (0, 0))
+
+	for event in pygame.event.get():
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			if event.button == 4:
+				#上滑
+				sp = sp+1000
+				print('上')
+			else:
+				#下滑
+				sp = sp-1000
+				print("下")
+		if event.type == pygame.QUIT:
+			camera.release()
+			pygame.quit()
+			exit()
+
+	for i in range(0,len(faceShow)):
+		et.show_text(screen,(width-50,10),str(f),(251,116,135),30)
+		roj = cv2.cvtColor(faceShow[i], cv2.COLOR_RGB2BGR)
+		roj = np.swapaxes(roj, 0, 1)
+		roj = pygame.pixelcopy.make_surface(roj)
+		et.show_text(screen,(width,i*200+sp),str(i),(251,65,90),40)
+		screen.blit(roj, (width, i*200+sp))
+
+
+	if frames < history:
+		et.show_text(screen, (100,200),u"请离开镜头",(255, 255, 255), True,120)
+		et.show_text(screen, (110,320),u"背      景      建      模      中:  {}%".format(frames),
+			(255, 255, 255), True,40)
+
+		KNN=et.KNN_difference(frame,args["min_area"])
+		rect = et.people(frame) #人检查
+		dets = et.dlibFace(img)
+		face=et.face(gray) #脸
+		
+		if rect != [] or face != () or dets != None or KNN != []:
+
+			for x,y,w,h in rect:
+				pygame.draw.rect(screen,[247,0,34],[x,y,w-x,h-y],3)
+			for fx,fy,fw,fh in face:
+				pygame.draw.rect(screen,[255,149,0],[fx,fy,fw,fh],3)
+			for r in KNN:
+				x,y,w,h = et.wrap_digit(r)
+				pygame.draw.rect(screen,[106,243,62],[x,y,w,h],3)
+
+			#for i,d in enumerate(dets):
+				#pygame.draw.rect(screen,[163,0,22],[d.left(),d.top(),d.right(),d.bottom()],3)
+			continue
+		else:
+			et.writeBackgroud(frame,frames)
+			frames += 1
+			continue
+	
+	#识别开始
+	#差度
+	#backgrouds=et.readBackgroud(random.randint(0,19))
+	#contours=et.frame_difference(backgrouds,gray)
+	#KNN
+	KNN=et.KNN_difference(frame,args["min_area"])
+	if  KNN != []:
+
+		#rect = et.people(frame) #人检查
+		face=et.face(gray) #脸
+
+		if face != ():
+			for fx,fy,fw,fh in face:
+				pygame.draw.rect(screen,[255,149,0],[fx,fy,fw,fh],3)
+				roi = gray[fy:fy+fh,fx:fx+fw]
+				roj = frame[fy:fy+fh,fx:fx+fw]
+				roi = cv2.resize(roi,(200,200))
+				roj = cv2.resize(roj,(200,200))
+
+				if f<20:
+					cv2.imwrite('face/face_gray/1/%s.png' % str(f),roi)
+					cv2.imwrite('face/face_color/1/%s.png' % str(f),roj)
+
+					faceShow.append(roj)
+					f =f+1
+					
+					
+				#screen.blit(roi, (0, 0))
+
+
+				#faceName=et.face2(roi)
+				#print(faceName)
+				for i in range(0,len(faces)):
+					for j in range(0,len(faces[i])):
+						pygame.draw.rect(screen,[193,133,47],[fx+fw,fy+(42*j),90,40])
+						et.show_text(screen,(fx+fw,fy+(45*j)),faces[i][j],(255,255,255), True,30)
+		
+	
+		
---
+++ interesion_model.py
@@ -0,0 +1,230 @@
+#interesion_model.py
+import cv2
+import dlib
+import time
+import datetime
+import imutils
+import numpy as np 
+import pygame
+from imutils.object_detection import non_max_suppression
+
+class exciting(object):
+	"""docstring for """
+	def __init__(self,camera,history):
+
+		#初始化方向梯度直方图描述子/设置支持向量机
+		self.hog = cv2.HOGDescriptor()
+		self.hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+		#初始化背景分割器
+		self.bg = cv2.createBackgroundSubtractorKNN(detectShadows=True)
+		self.bg.setHistory(history)
+		self.detector = dlib.get_frontal_face_detector()
+		self.face_cascade = cv2.CascadeClassifier('haarcascades//haarcascade_frontalface_default.xml')
+
+		self.model = None
+		self._camera = camera
+		self._videoWriter = None
+		self._videoFilename = None
+		self._framesElapsed = None
+		self._fpsEstimate = None
+		self._startTime = None
+		self._videoEncoding = None
+
+	def inside(self,r1,r2):
+		x1,y1,w1,h1 = r1
+		x2,y2,w2,h2 = r2
+		if (x1>x2) and (y1>y2) and (x1+w1<x2+w2) and (y1+h1<y2+h2):
+			return True
+
+		else:
+			return False 
+
+	def show_text(self,screen, pos, text, color, font_bold = False, font_size = 50, font_italic = False):   
+		'''
+		Function:文字处理函数 
+		Input:screen：surface句柄
+				pos：文字显示位置 
+				color:文字颜色 
+				font_bold:是否加粗 
+				font_size:字体大小 
+				font_italic:是否斜体 
+		Output: NONE '''
+		cur_font = pygame.font.SysFont("SourceHanSans-Bold", font_size)
+		cur_font.set_bold(font_bold)  
+		cur_font.set_italic(font_italic)  
+		text_fmt = cur_font.render(text, 1, color)  
+		return screen.blit(text_fmt, pos)
+
+	def wrap_digit(self,rect):
+		x,y,w,h = rect
+		padding = 5
+		hcenter = x+w/2
+		vcenter = y+w/2
+		if(h>w):
+			w = h
+			x = hcenter - (w/2)
+		else:
+			h = w
+			y = vcenter - (w/2)
+		return (x-padding,y-padding,w+padding,h+padding)
+
+
+	def readBackgroud(self,count):
+		return cv2.imread('Background//%s.png' % str(count),cv2.IMREAD_COLOR)
+
+	def writeBackgroud(self,frame,count):
+		#(帧，张)
+		return cv2.imwrite('Background/%s.png' % str(count),frame)
+
+	def writeImg(self,frame,path,count):
+		return cv2.imwrite(path+'/%s.png' % str(count),frame)
+
+	def read_images_array(self,path='face/face_gray/',l=20,z=0):
+	#读取图片（路径，张数，人数，图片太大了）
+	#face/str(1)/str(i)+'png'
+
+		c=0
+		x,y=[],[]
+		for o in range(0,z+1):
+			for i in range(0,l+1):
+				try:
+					path_in = path +str(o+1) +'/'+ str(i) + '.png'
+					im = cv2.imread(path_in,cv2.IMREAD_GRAYSCALE)
+					im = imutils.resize(im,32,32)
+				#im = cv2.resize(im,(100,100),interpolation = cv2.INTER_LINEAR)
+				
+					x.append(np.asarray(im,dtype=np.uint8))
+					y.append(c)
+					path_in = None
+				except:
+					continue
+			c=c+1
+		return [x,y]
+
+	def face_rec(self,array):
+		try:
+			[x,y] = array
+			y=np.asarray(y,dtype=np.int32)
+			self.model = cv2.face.createEigenFaceRecognizer()
+			self.model.train(np.asarray(x),np.asarray(y))
+		except:
+			return False
+
+	def face2(self,roi):
+		try:
+			roi = cv2.resize(roi,(32,32),interpolation = cv2.INTER_LINEAR)
+			return self.model.predict(roi)
+		except:
+			return
+
+	def frame_difference(self,firstFrame,gray):
+
+		avg = cv2.cvtColor(firstFrame,cv2.COLOR_BGR2GRAY)
+
+		gray = cv2.GaussianBlur(gray,(21,21),0)
+		avg = cv2.GaussianBlur(avg,(21,21),0)
+
+		#cv2.accumulateWeighted(gray,avg,0.5)
+		frameDelta =cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+		thresh =cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+		# 扩展阀值图像填充孔洞，然后找到阀值图像上的轮廓
+		thresh =cv2.dilate(thresh, None, iterations=2)
+		(_,cnts, _) =cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+		return cnts
+
+	def KNN_difference(self,frame,min_area=500):
+
+		fgmask = self.bg.apply(frame)
+		thresh = cv2.threshold(fgmask, 25, 255, cv2.THRESH_BINARY)[1]
+	
+		thresh = cv2.dilate(thresh, None, iterations=2)
+		#findContours(image, mode, method[, contours[, hierarchy[, offset]]]) -> image, contours, hierarchy
+		(_,cnts,_)= cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+		rectangles = []
+
+		for c in cnts:
+			if cv2.contourArea(c) < min_area:
+				continue
+			r = (x, y, w, h) = cv2.boundingRect(c)
+
+			is_inside = False
+			for q in rectangles:
+				if self.inside(r,q):
+					is_inside = True
+					break		
+			if not is_inside:
+				rectangles.append(r)
+
+		return rectangles
+
+
+	def people(self,frame):
+		#人
+		#调整到（1）减少检测时间，将图像裁剪到最大宽度为400个像素
+		frame = imutils.resize(frame, width=min(400, frame.shape[1]))
+		#检测图像中的人
+		(rects, weights) = self.hog.detectMultiScale(frame, winStride=(4, 4),
+			padding=(8, 8), scale=1.05)
+		# 应用非极大值抑制的边界框
+		# 相当大的重叠阈值尽量保持重叠
+		rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+		return non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+	def face(self,gray):
+		#脸
+		return self.face_cascade.detectMultiScale(gray, 1.3, 5)
+
+	def dlibFace(self,frame):
+		l = None
+		dets = self.detector(frame, 0)
+		for i,d in enumerate(dets):
+			#pygame.draw.rect(screen,[163,0,22],[d.left(),d.top(),d.right(),d.bottom()],3)
+			l = d.left()
+		return l 
+	def car(self):
+		#汽车
+		pass
+
+	def Ev(self):
+		#电动车
+		pass
+
+	def bicycle(self):
+		#自行车
+		pass
+
+	def Monitor(self,path,frame,encoding = cv2.VideoWriter_fourcc('I','4','2','0')):
+		#更新FPS估计和相关变量。
+		if self._framesElapsed == 0:
+			self._startTime = time.time()
+		else:
+			timeElapsed = time.time() - self._startTime
+			self._fpsEstimate = self._framesElapsed/timeElapsed
+		self._framesElapsed += 1
+
+		self._videoFilename = path
+		self._videoEncoding = encoding
+
+		self._writeVideoFrame()
+
+	def _writeVideoFrame(self):
+			
+		if not self.isWritingVideo:
+			return
+		if self._videoWriter is None:
+			fps = self._camera.get(cv2.CAP_PROP_FPS)
+			if fps == 0.0:
+				if self._framesElapsed <20:
+					return
+				else:
+					fps = self._fpsEstimate
+			size = (int(self._camera.get(cv2.CAP_PROP_FRAME_WIDTH)),int(self._camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+
+			self._videoWriter = cv2.VideoWriter(self._videoFilename,self._videoEncoding,fps,size)
+		self._videoWriter.Write(self._frame)
+
+	@property #在写视频吗？
+	def isWritingVideo(self):
+		return self._videoFilename is not None
